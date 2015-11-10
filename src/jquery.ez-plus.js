8a1257ac341928579ab4a245bfbe026a92d5e5df
Igor Lino <i.lino@directica.pl>
2015-11-10 20:35:14 +0100

    Fixes #15: scroll zoom on square images

--- src/jquery.ez-plus.js
+++ src/jquery.ez-plus.js
@@ -1743,7 +1743,7 @@ if (typeof Object.create !== 'function') {
                     if (self.nzWidth > self.nzHeight) {
                         self.currentZoomLevel = self.newvaluewidth;
                     }
-                    if (self.nzHeight > self.nzWidth) {
+                    else if (self.nzHeight >= self.nzWidth) {
                         self.currentZoomLevel = self.newvaluewidth;
                     }
                 }
